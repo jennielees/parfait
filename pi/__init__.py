566edb80c29a7c1e6fc88b218d60fef657c91971
Jennie Lees <jennie@close.co>
2014-05-10 15:17:38 -0700

    default pin

--- pi/__init__.py
+++ pi/__init__.py
@@ -2,12 +2,14 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+DEFAULT_PIN = 8
 
 def receive_ping():
    print "Ping received"
+   send_high()
    return True
 
-def send_high(pin, duration=1):
+def send_high(pin=DEFAULT_PIN, duration=1):
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, GPIO.HIGH)
   time.sleep(duration)
