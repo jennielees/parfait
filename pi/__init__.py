16c8bcb25925cbd5ca32789901ffe657c78b4523
Jennie Lees <jennie@close.co>
2014-05-10 15:15:25 -0700

    Initial gpio connection

--- pi/__init__.py
+++ pi/__init__.py
@@ -1,3 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
 def receive_ping():
    print "Ping received"
    return True
+
+def send_high(pin, duration=1):
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, GPIO.HIGH)
+  time.sleep(duration)
+  GPIO.output(pin, GPIO.LOW)
